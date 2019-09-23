520256e0f2d00919fba9467ebce1bffae5cb220b
YangZhenyu1006 <l>
2019-09-23 19:50:37 +0800

    完成

---
+++ web_back/js/utis/user.js
@@ -0,0 +1,35 @@
+var user ={
+    /**
+     * 
+     * @param {*} userName 用户名
+     * @param {*} password 密码
+     * @param {*} callBack 回调函数
+     */
+    login:function(userName,password,callBack){
+        //ajax请求
+        $.post(URLIST.user_login,{
+            user_name : userName,
+            password : password
+        },
+        function(y){
+            // y是响应体，是由、$.post决定的
+            callBack(y);
+        });
+    },
+    /**
+     * 用户退出
+     * @param {*} callBack 
+     */
+    logout:function(callBack){
+       $.post(URLIST.user_logout,function(y){
+        callBack(y)
+       }) ;
+    },
+    
+    getInfo:function(callBack){
+        $.get(URLIST.user_getInfo,function(res){
+        callBack(res);
+        });
+    }
+
+};
