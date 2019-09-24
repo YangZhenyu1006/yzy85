f79f98c663ba8a3b4d86382f56e85de3bbc4439d
YangZhenyu1006 <l>
2019-09-24 10:15:58 +0800

    编辑完成

--- web_back/js/utis/category.js
+++ web_back/js/utis/category.js
@@ -3,19 +3,23 @@
  */
 var category = {
     show:function(callBack){
-        $.get("http://localhost:8000/admin/category_search",function(res){
+        $.get(URLIST.category_show,function(res){
             callBack(res);
         });
     },
     add:function(name,slug,callBack){
-        $.post("http://localhost:8000/admin/category_add",{name:name,slug:slug},function(res){
+        $.post(URLIST.category_add,{name:name,slug:slug},function(res){
             callBack(res);
         });
     },
-    del:function(){
-
+    del:function(id,callBack){
+        $.post(URLIST.category_delete,{id:id},function(res){
+            callBack(res);
+        });
     },
-    edit:function(){
-
+    edit:function(id,name,slug,callBack){
+        $.post(URLIST.category_edit,{id:id,name:name,slug:slug},function(res){
+            callBack(res);
+        });
     }
 }
